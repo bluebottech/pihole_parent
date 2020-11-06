4bcdeead38f6c134813a6319abe8c9665799fbc4
bluebottech <72706986+bluebottech@users.noreply.github.com>
2020-11-05 21:52:09 -0500

    Create block.sh

---
+++ block.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+/usr/local/bin/pihole --wild discordapp.com discord.com discord.gg discordapp.net roblox.com rbxcdn.com gamejolt.com steam.com steampowered.com minecraft.de minecraft.com hypixel.net newgrounds.com
+
+/usr/local/bin/pihole restartdns
